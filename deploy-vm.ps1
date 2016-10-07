08afb5411b2881e5b21b7933c76f53c07b6a0380
Kylie Judd <kyliejudd@gmail.com>
2016-10-07 13:59:17 +0000

    adding check to wait for deployment. Adding cpu and memory config

--- deploy-vm.ps1
+++ deploy-vm.ps1
@@ -16,3 +16,18 @@ foreach ($vcenter in $vcenter){
 foreach ($deploy in $csv){
     get-cluster -name $deploy.cluster -server $deploy.vcenter | new-vm -Name $deploy.vmname -Datastore $deploy.datastore -template $deploy.template
 }
+
+
+#waiting for all the VM's to be provisioned 	
+do {
+    $toolsStatus = (Get-VM -name $csv.vmname).extensiondata.Guest.ToolsStatus
+    Start-Sleep -Seconds 10
+    } until  ($toolsStatus -match ‘toolsNotRunning’) 
+
+
+foreach($deploy in $csv){
+    get-vm -name $deploy.vmname | set-vm -numcpu $deploy.numcpu -confirm:$false 
+    start-sleep -Seconds 3
+    get-vm -name $deploy.vmname | set-vm -MemoryGB $deploy.memGB -confirm:$false 
+    }
+
