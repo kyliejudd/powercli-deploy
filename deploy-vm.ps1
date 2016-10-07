b93aab0aa0c5bbdc2da54716f636026f6d2d795e
Kylie Judd <kyliejudd@gmail.com>
2016-10-07 13:27:34 +0100

    initial commit

---
+++ deploy-vm.ps1
@@ -0,0 +1,18 @@
+﻿#collect variables from user 
+$vcentercred = (Get-Credential) 
+$csvinput =  
+$csv = import-csv $csvinput -UseCulture
+
+#import vmware powershell snapin
+add-pssnapin vmware* 
+
+#connect to each vcenter
+$vcenter = $csv.vcenter | select -Unique 
+foreach ($vcenter in $vcenter){
+    connect-viserver -Credential $vcentercred -Server $vcenter -WarningAction SilentlyContinue
+    }
+
+
+foreach ($deploy in $csv){
+    get-cluster -name $deploy.cluster -server $deploy.vcenter | new-vm -Name $deploy.vmname -Datastore $deploy.datastore -template $deploy.template
+}
