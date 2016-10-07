4c1634cc3c75b384a7cb2bac5c9b636ca54ee837
Kylie Judd <kyliejudd@gmail.com>
2016-10-07 13:48:53 +0100

    deploy script for portgroups and csv example

---
+++ deploy-vdportgroup.ps1
@@ -0,0 +1,7 @@
+﻿$csv = 
+$vdportgroup = Import-Csv -UseCulture
+
+foreach ($vdportgroup in $vdportgroups){ 
+    get-vdswitch $vdportgroup.vdswitch -location $vdportgroup.location | New-VDPortgroup -Name `
+    $vdportgroup.Name -VlanId $vdportgroup.vlanid
+    }
